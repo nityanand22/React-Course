5b522f8e5da5ef2ff793f34ea8ebf3c158282b89
Nityanand <you@example.com>
2024-10-28 23:01:17 +0530

    mini changes

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent)
+// const parent = React.createElement("div", {id: "parent"}, [
+//   React.createElement("div", {id: "child"}, [
+//     React.createElement("h1", {}, "This is h1 tag"),
+//     React.createElement("h2", {}, "This is h1 tag"),
+//   ]),
+//   React.createElement("div", {id: "child2"}, [
+//     React.createElement("h1", {}, "This is h1 tag"),
+//     React.createElement("h2", {}, "This is h1 tag"),
+//   ]),
+// ])
+// console.log(parent)
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(parent)
+
+console.log("hello world")
