253563346fc2dbd5df0425197a0efde698f05d8f
Nityanand <you@example.com>
2024-11-11 22:23:58 +0530

    used Link tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { FOOD_LOGO } from "../utils/constants";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
@@ -10,10 +11,18 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="cart">Cart</Link>
+          </li>
           <button
             className="login"
             onClick={() => {
