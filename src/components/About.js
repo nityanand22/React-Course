67fa93d6bf0f0174f8c83e3842f1e14403878639
Nityanand <you@example.com>
2024-12-10 14:31:52 +0530

    life cycle of class component means when componentdidmount will be called

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,25 @@ import ClassUser from "./UserClass";
 import { Component } from "react";
 
 class About extends Component {
+  constructor(props) {
+    super(props);
+
+    console.log("About constructor");
+  }
+
+  componentDidMount() {
+    console.log("About Component did mount");
+  }
+
   render() {
+    console.log("About render");
     return (
       <div>
         <h1>About Page</h1>
         <p>This is the about page</p>
         <ClassUser name={"Nityanand"} location={"Nashik"} />
+        <ClassUser name={"Elon Musk"} location={"USA"} />
+        <ClassUser name={"Jeff Bezos"} location={"US"} />
       </div>
     );
   }
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,6 +4,8 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
 
+    console.log(props.name + " constructor");
+
     this.state = {
       count1: 0,
       count2: 1,
@@ -11,6 +13,7 @@ class UserClass extends React.Component {
   }
 
   componentDidMount() {
+    console.log(this.props.name + " component did mount");
     // called in the last
     // used for make api call in class based component  because in the functional component there is useEffect for calling api
   }
@@ -19,6 +22,7 @@ class UserClass extends React.Component {
     const { name } = this.props;
     const { count1, count2 } = this.state;
 
+    console.log(this.props.name + " render");
     return (
       <div className="user-card">
         <p>Count : {count1}</p>
