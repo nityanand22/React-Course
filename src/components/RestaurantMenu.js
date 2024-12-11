2acee612e01e69691da25406d95f0ecce3d37fe6
Nityanand <you@example.com>
2024-12-11 13:58:48 +0530

    created custom hooks useRestaurantEmenu for call api seperately , useOnlineStatus for check online & used in header, created Grocery component and used lazy(used to create seperate bundle for grocery) and used Suspense which helps to load component, used online in body component

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,37 +1,25 @@
-import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
-
   const { resId } = useParams();
-  console.log(resInfo);
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    const data = await fetch(MENU_API + resId);
-    const json = await data.json();
-    setResInfo(json.data);
-  };
+  const resInfo = useRestaurantMenu(resId);
 
   if (resInfo === null) return <Shimmer />;
 
-  const resName = resInfo?.cards[0]?.card?.card?.text;
+  const resName = resInfo?.data?.cards[2]?.card?.card?.info?.name;
 
   const { itemCards } =
-    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+    resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+      ?.card;
 
   return (
     <div className="menu">
       <h1>{resName}</h1>
       <h3>Menu</h3>
       <ul>
-        <li>{itemCards[0].card.info.name}</li>
         {itemCards.map((item) => (
           <li key={item.card.info.id}>
             {item.card.info.name} - {item.card.info.price / 100} Rs.
